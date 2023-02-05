1971d23571ebbe8c4893a1cb4d3871912380ee6a
vikharevam <103068846+vikharevam@users.noreply.github.com>
2023-02-05 15:22:40 +0300

    Initial Commit

---
+++ Quizz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizz
+//
+//  Created by Александр Вихарев on 05.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
