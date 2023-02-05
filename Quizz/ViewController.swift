3500334b6c94a05ed4678dfaf59c5621f405ba8b
vikharevam <103068846+vikharevam@users.noreply.github.com>
2023-02-05 19:31:36 +0300

    Adding questions (test)

--- Quizz/ViewController.swift
+++ Quizz/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionView: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quizzQuestoins = ["Four + Two is equal to Six.",
+                          "Five - Three is greater than One",
+                          "Three + Eight is less than Ten"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        questionView.text = quizzQuestoins[Int.random(in: 0...2)]
     }
 
 
+    @IBAction func amswerPassedButton(_ sender: Any) {
+    }
 }
 
